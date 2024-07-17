257c1b8c11ae374f9098284909cb7963262d0c97
Robert Godlewski <88469054+robert-godlewski@users.noreply.github.com>
2024-07-17 14:53:23 -0700

    copied in tutorial stuff into existing repo (#2)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""gpsite URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/register/', CreateUserView.as_view(), name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
