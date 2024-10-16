f6390a1a280fafec120476832edfcadf79266fbc
Robert Godlewski <robtechg5@gmail.com>
2024-10-16 15:37:27 -0700

    added in a landing page for newcommers

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+function Landing() {
+    return <div>
+        {/* Add in welcoming information here */}
+        <div>
+            <a href="/register">Need to make an account?</a>
+            <a href="/login">Loggin into an existing account.</a>
+        </div>
+        {/* Add in all of the categories and post views here */}
+    </div>
+}
+
+export default Landing;
