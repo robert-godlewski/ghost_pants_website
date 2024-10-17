e1007dddf75b935bc495823e4dbc2977b3448f3c
Robert Godlewski <88469054+robert-godlewski@users.noreply.github.com>
2024-10-17 15:00:50 -0700

    Adding in a basic landing page (#30)
    * updated database so that categories and posts automatically make a slug
    
    * added in landing page and fixed some routes
    
    * fixed typo on landing page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+function Landing() {
+    return <div>
+        {/* Add in welcoming information here */}
+        <p>
+            <a href="/register">Need to make an account?</a>
+            <span> or </span>
+            <a href="/login">Loging into an existing account.</a>
+        </p>
+        {/* Add in categories and post views that everyone can see here */}
+    </div>
+}
+
+export default Landing;
