205acb6386961035e913f309afba749d14959f17
Robert Godlewski <robtechg5@gmail.com>
2024-10-17 14:59:09 -0700

    fixed typo on landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,8 @@ function Landing() {
     return <div>
         {/* Add in welcoming information here */}
         <p>
-            <a href="/register">Need to make an account?</a> or 
+            <a href="/register">Need to make an account?</a>
+            <span> or </span>
             <a href="/login">Loging into an existing account.</a>
         </p>
         {/* Add in categories and post views that everyone can see here */}
