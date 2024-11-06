0012ce902a860a482a1094e912a1cec9074cc0a7
Robert Godlewski <robtechg5@gmail.com>
2024-11-05 17:30:18 -0800

    added in draft for update post page

---
+++ frontend/src/pages/UpdatePost.jsx
@@ -0,0 +1,18 @@
+// import {useState, useEffect} from 'react';
+// import {useNavigate, useParams} from 'react-router-dom';
+// import api from '../api';
+
+function UpdatePost() {
+    // const {slug} = useParams();
+    // const [post, setPost] = useState({});
+
+    // const navigate = useNavigate();
+
+    // Replace this with a form instead with post data here from the api
+    return <div>
+        <p>Update Post</p>
+        <a href="/">Cancel</a>
+    </div>
+}
+
+export default UpdatePost;
